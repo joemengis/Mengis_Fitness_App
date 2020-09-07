afb8bbfe7fe5914d7d17a70668f6fceb32120f9e
LJFrancis <joe.mengis@gmail.com>
2020-09-06 21:24:39 -0700

    version one complete

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,34 +1,48 @@
 const express = require("express");
 const router = require("express").Router();
 
-const db = require('../models');
-
+const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({}).then(exercises => {
-    console.log("exercises are :", exercises);
+  db.Workout.find({}).then((exercises) => {
+      exercises.map((exercise) =>
+        console.log("exercises field", exercise.exercises)
+      );
+      res.json(exercises);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  console.log("we are in the get/range api route");
+  db.Workout.find({})
+    .limit(7).then((exercises) => {
+      res.json(exercises);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({body, params}, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {$push: {exercises: body}},
+    {new: true}
+  ).then((exercises) => {
+    console.log("Here is what got put in put route", exercises);
     res.json(exercises);
-  }).catch(err=>{
-    res.json(err);
-  });  
-});
-
-router.put("/api/workouts/:workout", ({ params, body }, res) => {
-  db.Workout.findOneAndUpdate(
-    {_id: params.id},
-    {$push: { excercises: body } },
-    {upsert: true, useFindandModify: false },
-    (updatedWorkout) => {
-      res.json(updatedWorkout);
-    }
-  );
-});
-
-router.post("/api/workouts", (req, res) => {
-  db.Workout.create({}).then((newWorkout) => {
-    res.json(newWorkout);
   });
 });
 
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create({}).then((exercises) => res.json(exercises))
+    .catch((err) => {
+      console.log("err", err);
+      res.json(err);
+    });
+});
 
 module.exports = router;
